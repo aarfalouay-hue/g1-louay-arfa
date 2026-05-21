702f4bba22d675319f88bdda969f87abf3154b5c
aarfalouay-hue <aarfalouay@gmail.com>
2026-05-21 22:22:18 +0100

    week 4

--- src/App.jsx
+++ src/App.jsx
@@ -15,26 +15,55 @@ const stories = [
     points: 95,
     num_comments: 30
   }
-]
+];
+
 function App() {
   return (
-    
+    <div>
+      <Header />
+      <Search />
+      <List />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <header>
+      <h1>Hacker News Clone</h1>
+    </header>
+  );
+}
+
+function Search() {
+  return (
+    <div>
+      <label htmlFor="search">Search:</label>
+      <input id="search" type="text" />
+    </div>
+  );
+}
+
+function List() {
+  return (
     <div>
       {stories.map((story) => {
         return (
-          /*title as a link to the story's URL*/
-        <div key={story.objectID}>
-  <h3>
-    <a href={story.url} target="_blank" rel="noreferrer">
-      {story.title}
-    </a>
-  </h3>
+          <div key={story.objectID}>
+            <h3>
+              <a href={story.url} target="_blank" rel="noreferrer">
+                {story.title}
+              </a>
+            </h3>
 
-  <p>Author: {story.author}</p>
-  <p>Points: {story.points}</p>
-  <p>Comments: {story.num_comments}</p>
-</div>);
+            <p>Author: {story.author}</p>
+            <p>Points: {story.points}</p>
+            <p>Comments: {story.num_comments}</p>
+          </div>
+        );
       })}
     </div>
   );
-};
+}
+
+export default App;
